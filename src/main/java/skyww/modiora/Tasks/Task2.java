88d89b82629153fa6dec9d39b4b5b83d529f4930
skywalker <wsky3478@gmail.com>
2026-06-23 19:14:42 +0300

    add Task 2

---
+++ src/main/java/skyww/modiora/Tasks/Task2.java
@@ -0,0 +1,9 @@
+package skyww.modiora.Tasks;
+
+public class Task2 {
+    public static void main(String[] args) {
+        System.out.println("Artur");
+        System.out.println(20);
+        System.out.println("Java");
+    }
+}
