5cdafcffca424d64cbf2ae5624ddbc9532d4cef4
skywalker <wsky3478@gmail.com>
2026-06-23 19:27:49 +0300

    add Task 3

---
+++ src/main/java/skyww/modiora/Tasks/Task3.java
@@ -0,0 +1,21 @@
+package skyww.modiora.Tasks;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("Artur");
+        System.out.println(123*456);
+        System.out.println("*".repeat(10));
+
+        System.out.print("слово 1");
+        System.out.print("слово 2");
+        System.out.print("слово 3");
+    }
+}
+
+/*
+Результат:
+Artur
+56088
+**********
+123
+*/
